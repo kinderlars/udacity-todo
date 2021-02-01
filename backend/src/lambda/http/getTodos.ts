c0176232dcf554a306040df2853ddfa1a18952d2
Lars Kinder <lars.kinder@porsche.de>
2021-02-01 08:45:02 +0100

    Added getTodos functionality and added authorizer to function

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,13 +1,30 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+import * as XRay from 'aws-xray-sdk'
+import {createLogger} from "../../utils/logger";
+
+const logger = createLogger('getTodos')
+
+const XAWS = XRay.captureAWS(AWS)
+
+const docClient = new XAWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.info(`Processing event ${event}`)
   // TODO: Get all TODO items for a current user
-  const test = event
+  const todos = await docClient.scan({
+    TableName: todosTable
+  }).promise()
+
+  const items = todos.Items
 
   return{
     statusCode: 200,
-    body: JSON.stringify(test)
+    body: JSON.stringify({
+      items
+    })
   }
 }
