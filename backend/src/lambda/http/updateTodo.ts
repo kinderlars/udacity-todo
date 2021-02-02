d4a12b778d6d2d35ec48d9e1bdf345fd6b5bd52c
Lars Kinder <lars.kinder@porsche.de>
2021-02-02 11:18:06 +0100

    Added update functionality and fixed bugs

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -3,15 +3,30 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import {createLogger} from "../../utils/logger";
+import {parseUserId} from "../../auth/utils";
+import {updateTodo} from "../../businessLayer/todos";
+
+const logger = createLogger('getTodos')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  console.log(todoId)
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+  const userId = parseUserId(jwtToken)
+
+  logger.info(updatedTodo)
+
+  const result = await updateTodo(todoId,userId,updatedTodo)
+
   return {
     statusCode:200,
-    body: JSON.stringify(updatedTodo)
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(result)
   }
 }
