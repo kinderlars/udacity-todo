30c9d6031e9c63f2f0e5cf46e2f8c9b31905df99
Lars Kinder <lars.kinder@porsche.de>
2021-02-02 16:05:00 +0100

    Added client config and update gitignore

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,9 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'k56y4keese'
+const region = 'eu-central-1'
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-lb67h78z.eu.auth0.com',            // Auth0 domain
+  clientId: 'QYj31wXTLjIMbDqwxvow3VulPPgO2blb',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
