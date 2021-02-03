f39b451aca94fa1e38b600ae8f2b4996e3192a4e
Lars Kinder <lars.kinder@porsche.de>
2021-02-03 07:22:03 +0100

    Modified create todo return value

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -28,6 +28,8 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
-    body: JSON.stringify(result)
+    body: JSON.stringify({
+      item: result
+    })
   }
 }
